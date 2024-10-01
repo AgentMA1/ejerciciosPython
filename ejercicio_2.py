08dd1db0f31f188d04c030254d893e38845ec76f
AgentMA1 <notsero22@gmail.com>
2024-10-01 11:07:03 +0000

    Ejer.2

---
+++ ejercicio_2.py
@@ -0,0 +1,10 @@
+
+
+# ejercicio_2.py
+base = float(input("Enter the base of the rectangle: "))
+height = float(input("Enter the height of the rectangle: "))
+perimeter = 2 * (base + height)
+area = base * height
+print(f"The perimeter of the rectangle is {perimeter}")
+print(f"The area of the rectangle is {area}")
+
