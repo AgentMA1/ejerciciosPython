d68d07e13051669e6b64b24b83dcff622efc2d50
AgentMA1 <notsero22@gmail.com>
2024-10-01 11:26:59 +0000

    Ejer.7

---
+++ ejercicio_7.py
@@ -0,0 +1,6 @@
+# ejercicio_7.py
+minutes = int(input("Enter the number of minutes: "))
+hours = minutes // 60
+remaining_minutes = minutes % 60
+print(f"{minutes} minutes are {hours} hours and {remaining_minutes} minutes")
+
