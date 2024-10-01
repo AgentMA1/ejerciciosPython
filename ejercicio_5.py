b2e8a33700db34c5d2a5a68d49efa5c977258364
AgentMA1 <notsero22@gmail.com>
2024-10-01 11:17:53 +0000

    Ejer.5

---
+++ ejercicio_5.py
@@ -0,0 +1,5 @@
+# ejercicio_5.py
+fahrenheit = float(input("Enter the temperature in degrees Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5 / 9
+print(f"The temperature in degrees Celsius is {celsius}")
+
