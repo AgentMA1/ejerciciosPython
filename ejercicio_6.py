d4c254487edb9f8a3d148a366d7e9ba72b4066d3
AgentMA1 <notsero22@gmail.com>
2024-10-01 11:24:27 +0000

    Ejer.6

---
+++ ejercicio_6.py
@@ -0,0 +1,7 @@
+# ejercicio_6.py
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+average = (num1 + num2 + num3) / 3
+print(f"The average of the three numbers is {average}")
+
