99ad6b22382ee392541a029e96cc351bcd834373
AgentMA1 <notsero22@gmail.com>
2024-10-02 05:24:50 +0000

    ejer.9

---
+++ ejercicio_9.py
@@ -0,0 +1,6 @@
+# ejercicio_9.py
+total_purchase = float(input("Enter the total purchase amount: "))
+discount = total_purchase * 0.15
+total_to_pay = total_purchase - discount
+print(f"The total to pay with the discount is: {total_to_pay}")
+
