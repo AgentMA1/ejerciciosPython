eb59dd2778995cbfb510f7fce6ba3383240299d9
AgentMA1 <notsero22@gmail.com>
2024-10-02 11:16:08 +0000

    Ejer.26

---
+++ ejercicio_26.py
@@ -0,0 +1,8 @@
+# ejercicio_26.py
+start = int(input("Enter the start number: "))
+end = int(input("Enter the end number: "))
+
+for i in range(start, end + 1):
+    if i % 2 == 0:
+        print(i)
+
