3c07a606d1f0aed184ef69c88a0000b30eddfed1
AgentMA1 <notsero22@gmail.com>
2024-10-04 06:05:04 +0000

    ejer.39

---
+++ ejercicio_39.py
@@ -0,0 +1,20 @@
+# ejercicio_39.py
+while True:
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. Option 3")
+    print("4. Exit")
+    
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        print("You chose option 1")
+    elif choice == "2":
+        print("You chose option 2")
+    elif choice == "3":
+        print("You chose option 3")
+    elif choice == "4":
+        break
+    else:
+        print("Invalid option")
+
